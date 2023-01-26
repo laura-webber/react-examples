d8d428abb64c5aabbde7bd5ee4ddf10058fdc2cd
Laura Webber <93733302+laura-webber@users.noreply.github.com>
2023-01-26 14:54:33 -0800

    first commit

---
+++ examples/app-navbar/src/myapp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+          <Navbar name={this.state.name} />
+       </div>
+    ); 
+  }
+};
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+}
